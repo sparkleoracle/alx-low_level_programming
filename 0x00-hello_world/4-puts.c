9c8f27d17bf5d5313f24cafe1bc20776a10080a3
sparkleoracle <frankgombosparkle@gmail.com>
2023-12-14 02:01:54 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is a main function
+ * Return: always 0
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}

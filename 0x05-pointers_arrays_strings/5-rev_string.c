e114e996b9c4a1d58cb43e88239363c09fcac18a
sparkleoracle <frankgombosparkle@gmail.com>
2024-01-03 17:46:38 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - the function name string
+ * that reverse the string
+ * @s: function parameter
+ */
+
+void rev_string(char *s)
+{
+int i;
+int count = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+	count++;
+for (i = 0; i < count / 2; i++)
+{
+char j;
+j = s[i];
+	s[i] = s[count - 1 - i];
+	s[count - 1 - i] = j;
+}
+
+}

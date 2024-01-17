17cffe6bca504bb70b8ea21661db80e0c65abba3
sparkleoracle <frankgombosparkle@gmail.com>
2024-01-17 03:13:58 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,83 +1,46 @@
 #include <stdio.h>
-	#include <stdlib.h>
-	#include <ctype.h>
-	#include <string.h>
-	/**
-	 * check_num - check - string there are digit
-	 * @str: array str
-	 *
-	 * Return: Always 0 (Success)
-	 */
-	int check_num(char *str)
+#include <stdlib.h>
+#include <ctype.h>
+#define UNUSED(x)(void)(x)
+/**
+ * main - adds two numbers
+ * @argc:int
+ * @argv:array of strings
+ * Return: sum if all digits
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+	int c;
+	int sum;
+	char *s;
+
+	sum = 0;
+	c = 0;
+
+	i = 1;
+	while (i < argc)
 	{
-		/*Declaring variables*/
-		unsigned int count;
-
-
-		count = 0;
-		while (count < strlen(str)) /*count string*/
-
-
+		s = argv[i];
+		j = 0;
+		while (s[j] != '\0')
 		{
-			if (!isdigit(str[count])) /*check if str there are digit*/
-			{
-				return (0);
-			}
-
-
-			count++;
-		}
-		return (1);
-	}
-
-
-	/**
-	 * main - Print the name of the program
-	 * @argc: Count arguments
-	 * @argv: Arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
-
-
-	int main(int argc, char *argv[])
-
-
-	{
-
-
-		/*Declaring variables*/
-		int count;
-		int str_to_int;
-		int sum = 0;
-
-
-		count = 1;
-		while (count < argc) /*Goes through the whole array*/
-		{
-			if (check_num(argv[count]))
-
-
-			{
-				str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
-				sum += str_to_int;
-			}
-
-
-			/*Condition if one of the number contains symbols that are not digits*/
-			else
+			if (s[j] < '0' || s[j] > '9')
 			{
 				printf("Error\n");
 				return (1);
 			}
-
-
-			count++;
+			j++;
 		}
-
-
-		printf("%d\n", sum); /*print sum*/
-
-
-		return (0);
+		i++;
 	}
+
+	for (i = 1; i < argc; i++)
+	{
+		c = atoi(argv[i]);
+		sum = sum + c;
+	}
+	printf("%d\n", sum);
+	return (0);
+}

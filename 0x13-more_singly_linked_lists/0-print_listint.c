03fb6c7794d49b27435bdadff0397779c48ad5c8
sparkleoracle <frankgombosparkle@gmail.com>
2024-02-06 02:29:04 -0800

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t j = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		num++;
 		h = h->next;
+		++j;
 	}
-	return (num);
+	return (j);
 }

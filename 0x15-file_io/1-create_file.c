1bcdd2b87b3f03ab3ae1f153f7a73c13b9b48bc5
sparkleoracle <frankgombosparkle@gmail.com>
2024-02-15 00:57:25 -0800

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_file - function that creates a file.
+ * @filename: variable pointer
+ * @text_content: content file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int j = 0, file_d;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+
+	while (text_content[j] != '\0')
+	{
+		j++;
+	}
+
+	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file_d == -1)
+		return (-1);
+
+	write(file_d, text_content, j);
+
+	return (1);
+}
